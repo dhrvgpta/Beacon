2f0ab1f60cc0b9528e45a1f795fbfe308e302ea1
dhruv <dhrvgpta@gmail.com>
2022-06-20 22:37:20 +0530

    Initial Commit

---
+++ Beacon/BeaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconApp.swift
+//  Beacon
+//
+//  Created by Dhruv Gupta on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct BeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
